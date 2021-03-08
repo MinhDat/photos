18f61c2af47a061773c10ed92e3f2b2db1449b8c
Hà Đạt <hadat@Has-iMac.local>
2021-03-08 22:44:53 +0700

    config deployment

--- src/component/Navigation/index.js
+++ src/component/Navigation/index.js
@@ -3,21 +3,23 @@ import Search from "component/Search";
 import { Link } from "react-router-dom";
 import "./style.scss";
 
+const SUB_DOMAIN = process.env.NODE_ENV === "production" ? "/photos" : "";
+
 export default function Navigation({ setSearchText }) {
   return (
     <div className="nav-wrap flex spaceBetween">
       <div className="nav-left-wrap flex itemCenter">
-        <Link to="/photos/" className="nav-link flex itemCenter">
+        <Link to={`${SUB_DOMAIN}/`} className="nav-link flex itemCenter">
           <img
             className="nav-logo"
             src={require("resources/img/logo.svg").default}
           />
           <h1 className="nav-title">Photos</h1>
         </Link>
-        <Link to="/like" className="nav-link">
+        <Link to={`${SUB_DOMAIN}/like`} className="nav-link">
           <p>Likes</p>
         </Link>
-        <Link to="/delete" className="nav-link">
+        <Link to={`${SUB_DOMAIN}/delete`} className="nav-link">
           <p>Deletes</p>
         </Link>
       </div>
